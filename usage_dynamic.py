c103a46547e1060ba40fb4e22033993de043566c
lpawlick <lpawlick@users.noreply.github.com>
2023-08-09 02:38:03 +0200

    Added a usage example for using a function to authenticate

---
+++ usage_dynamic.py
@@ -0,0 +1,43 @@
+from dash import Dash, Input, Output, dcc, html
+import dash_auth
+
+def authenticate(username, password):
+    return username == 'hello' and password == 'world'
+    
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+auth = dash_auth.BasicAuth(
+    app,
+    username_password_list=authenticate
+)
+
+app.layout = html.Div([
+    html.H1('Welcome to the app'),
+    html.H3('You are successfully authorized'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['A', 'B']],
+        value='A'
+    ),
+    dcc.Graph(id='graph')
+], className="container")
+
+
+@app.callback(Output('graph', 'figure'), [Input('dropdown', 'value')])
+def update_graph(dropdown_value):
+    return {
+        'layout': {
+            'title': 'Graph of {}'.format(dropdown_value),
+            'margin': {
+                'l': 20,
+                'b': 20,
+                'r': 10,
+                't': 60
+            }
+        },
+        'data': [{'x': [1, 2, 3], 'y': [4, 1, 2]}]
+    }
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
